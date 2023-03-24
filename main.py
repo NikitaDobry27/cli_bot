1a002fa9702f2f1be3528bf1ca4270e75c33d5f7
NikitaDobry27 <bazhenov.nikita27@gmail.com>
2023-03-24 17:42:20 +0100

    Finished

--- main.py
+++ main.py
@@ -1,109 +1,160 @@
+import os
+import json
 import re
 
+FILE_NAME = 'contacts.json'
+
+if not os.path.exists(FILE_NAME):
+    with open(FILE_NAME, 'w') as file:
+        json.dump({}, file)
 
 
-contacts = {}
+def load_contacts():
+    with open(FILE_NAME, 'r') as file:
+        contacts = json.load(file)
+    return contacts
+
+
+def save_contacts(contacts):
+    with open(FILE_NAME, 'w') as file:
+        json.dump(contacts, file)
+
+
+contacts = load_contacts()
+
 
 def input_validation(func):
-    def wrapper(*args, **kwargs):
+    def wrapper(*args):
         try:
-            return func(*args, **kwargs)
+            return func(*args)
         except IndexError:
-            return 'Please provide name and phone number'
-        except TypeError:
-            return f"{args[0]} is not a valid command"
-
+            return '\nPlease provide name and phone number\n'
+        except KeyError:
+            return f'\nThere is no contact with such name\n'
+        except ValueError:
+            return '\nOnly name is required\n'
     return wrapper
 
 
 def input_formatter(str):
     words_from_string = re.findall(r'\b\w+\b', str)
     command = words_from_string[0]
+
     if len(words_from_string) == 1:
         return command, ''
     data = words_from_string[1:]
     return command, data
 
-@input_validation
 def command_handler(command):
     for func, word in COMMANDS.items():
         if command in word:
             return func
-    return lambda x: f"{command} is not a valid command"
+    return unknown_command
+
+
+def unknown_command(*args):
+    return "\nThis command doesn't exist. Please try again\n"
 
 @input_validation
-def hello(*args, **kwargs):
-    return 'Hi! How can I help you'
-
-@input_validation
-def add_contact(*args, **kwargs):
-    lst = args[0]
-    name = lst[0]
-    phone =lst[1]  
-    
-    if len(lst) < 2:
-        return 'name or phone number is missing'
-    if len(phone) > 15 or len(phone) < 9:
-        return f'{phone} is not valid'
-    contacts[name] = phone
-    return 'Contact successfully added'
+def hello(*args):
+    return '\nHi! How can I help you\n'
 
 
 @input_validation
-def change(*args, **kwargs):
+def add_contact(*args):
     lst = args[0]
     name = lst[0]
     phone = lst[1]
-    
-    if name in contacts.keys():
-        contacts[name] = phone
-        return f'Phone number for contact {name.capitalize()} was successfully changed'
 
-    else:
-        return f'There is no such contact with name {name}'
+    if len(lst) < 2:
+        raise IndexError
+    if len(phone) > 15 or len(phone) < 9:
+        return f'\n{phone} is not valid\n'
+    contacts[name] = phone
+    save_contacts(contacts)
+    return f'\nContact {name.capitalize()} was successfully added\n'
 
 
 @input_validation
-def phone(*args, **kwargs):
-    pass
+def change(*args):
+    lst = args[0]
+    name = lst[0]
+    phone = lst[1]
+
+    if name in contacts.keys():
+        contacts[name] = phone
+        save_contacts(contacts)
+        return f'\nPhone number for contact {name.capitalize()} was successfully changed\n'
+    else:
+        raise KeyError
 
 
+@input_validation
+def phone(*args):
+    lst = args[0]
+    if not lst:
+        return '\nPlease provide "phone" and contact name\n' 
+    if len(lst) > 1:
+        raise ValueError
+    name = lst[0]
 
-def show_all(*args, **kwargs):
+    if name in contacts.keys():
+        return f'\nPhone number for contact {name.capitalize()} is {contacts[name]}\n'
+
+    raise KeyError
+
+@input_validation
+def remove_contact(*args):
+    lst = args[0]
+    
+    if not lst:
+        return '\nPlease provide "remove" and contact name\n' 
+
+    if len(lst) > 1:
+        raise ValueError
+    name = lst[0]
+
+    if name in contacts.keys():
+        contacts.pop(name)
+        save_contacts(contacts)
+        return f'\n{name.capitalize()} was successfully removed from your contacts\n'
+
+    raise KeyError
+
+def show_all(*args):
     if not contacts:
-        return 'Your contacts list is empty'
+        return '\nYour contacts list is empty\n'
     result = ''
     for k, v in contacts.items():
         result += f'\n{k.capitalize()}: {v}\n'
     return result
 
+
 COMMANDS = {
-        hello: ['hi', 'hello', 'hey'],
-        add_contact: ['add'],
-        change: ['change'],
-        phone: ['phone'],
-        show_all: ['show']
-    }
+    hello: ['hi', 'hello', 'hey'],
+    add_contact: ['add'],
+    change: ['change'],
+    phone: ['phone'],
+    show_all: ['show'],
+    remove_contact: ['remove', 'delete']
+}
 
 
 def main():
+    print(hello())
     while True:
         user_input = input('Type your query >>> ').lower()
-        
-        if user_input == 'close':
+
+        if user_input in ['close', '.', 'bye', 'exit']:
             print('See you!')
-            return False
-        
+            break
+
         command, data = input_formatter(user_input)
 
         call = command_handler(command)
-        
-        
-        print(call(data))
-        
 
-    # add_contact(user_input)
-    # print(contacts)
+        print(call(data))
+
 
 if __name__ == "__main__":
     main()
